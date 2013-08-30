f8df7bf4a21e4de15082672f8b6a8d956f51df74
Chris Coleman <ccoleman@sandowmedia.com>
2013-08-30 10:22:54 -0600

    Set default template filters

--- tinymce/templates/tinymce/filebrowser.js
+++ tinymce/templates/tinymce/filebrowser.js
@@ -1,14 +1,22 @@
 function djangoFileBrowser(field_name, url, type, win) {
+    var filter_type = "";
+
+    switch(type) {
+        case 'image': filter_type = "Image"; break;
+        case 'media': filter_type = "Video"; break;
+    }
+    
     var url = "{{ fb_url }}?popup=1&pop=2&type=" + type;
 
-    tinyMCE.activeEditor.windowManager.open(
+    var nw = tinyMCE.activeEditor.windowManager.open(
         {
             'file': url,
             'width': 820,
             'height': 500,
             'resizable': "yes",
+            'movable': "yes",
             'scrollbars': "yes",
-            'inline': "no",
+            'inline': "yes",
             'close_previous': "no"
         },
         {
@@ -17,5 +25,6 @@ function djangoFileBrowser(field_name, url, type, win) {
             'editor_id': tinyMCE.activeEditor.id
         }
     );
+
     return false;
 }
